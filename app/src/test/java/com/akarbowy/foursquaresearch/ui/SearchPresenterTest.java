5ed77f1f9fa24907f35215f4788a6c779bfe7646
murdly <arkadiusz.karbowy@gmail.com>
2017-07-09 07:52:01 +0200

    more presenter tests

--- app/src/test/java/com/akarbowy/foursquaresearch/ui/SearchPresenterTest.java
+++ app/src/test/java/com/akarbowy/foursquaresearch/ui/SearchPresenterTest.java
@@ -19,30 +19,65 @@ import java.util.List;
 import static junit.framework.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SearchPresenterTest {
 
+    private static List<VenueItem> VENUES;
+
     @Mock
     SearchContract.View view;
-
     @Mock
     DataSource dataSource;
-
     @InjectMocks
     SearchPresenter presenter;
-
     @Captor
     ArgumentCaptor<DataSource.GetVenuesCallback> getVenuesCallback;
 
     @Before
     public void setUp() throws Exception {
         when(view.isActive()).thenReturn(true);
+
+        VENUES = new ArrayList<>();
+        VENUES.add(new VenueItem());
+        VENUES.add(new VenueItem());
+        VENUES.add(new VenueItem());
+    }
+    @Test
+    public void doesNotCallViewMethods_WhenNoView() {
+        String query = "dummy";
+
+        presenter.search(query);
+
+        verify(view, never()).setLoading(false);
     }
 
-    @Test public void showsEmptyState_WhenNoResults() {
+    @Test
+    public void showsList_WhenResults() {
+        String query = "dummy";
+
+        presenter.search(query);
+
+        InOrder inOrder = inOrder(view);
+        inOrder.verify(view).setLoading(true);
+
+        verify(dataSource).loadVenues(eq(query), getVenuesCallback.capture());
+        getVenuesCallback.getValue().onDataLoaded(VENUES);
+
+        inOrder.verify(view).setLoading(false);
+
+        ArgumentCaptor<List> setVenuesCaptor = ArgumentCaptor.forClass(List.class);
+        verify(view).setVenues(setVenuesCaptor.capture());
+        assertTrue(setVenuesCaptor.getValue().size() == VENUES.size());
+
+        verify(view).setEmptyState(false);
+    }
+
+    @Test
+    public void showsEmptyState_WhenNoResults() {
         String query = "dummy";
 
         presenter.search(query);
@@ -62,4 +97,20 @@ public class SearchPresenterTest {
         verify(view).setEmptyState(true);
     }
 
+    @Test
+    public void showsError_WhenNoDataAvailable() {
+        String query = "dummy";
+
+        presenter.search(query);
+
+        InOrder inOrder = inOrder(view);
+        inOrder.verify(view).setLoading(true);
+
+        verify(dataSource).loadVenues(eq(query), getVenuesCallback.capture());
+        getVenuesCallback.getValue().onDataNotAvailable();
+
+        inOrder.verify(view).setLoading(false);
+
+        verify(view).showError();
+    }
 }
